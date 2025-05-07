901c84970e9be25c0b00eb9a4c62079621964e46
mstfrog <namyouchan0@gmail.com>
2025-05-08 04:41:44 +0900

    Update base.c

--- base.c
+++ base.c
@@ -1,39 +1,39 @@
 
 /*  call_stack
 
-    ½ÇÁ¦ ½Ã½ºÅÛ¿¡¼­´Â ½ºÅÃÀÌ ¸Ş¸ğ¸®¿¡ ÀúÀåµÇÁö¸¸, º» °úÁ¦¿¡¼­´Â `int` ¹è¿­À» ÀÌ¿ëÇÏ¿© ¸Ş¸ğ¸®¸¦ ±¸ÇöÇÕ´Ï´Ù.
-    ¿ø·¡´Â SFP¿Í Return Address¿¡ ½ÇÁ¦ °¡»ó ¸Ş¸ğ¸® ÁÖ¼Ò°¡ µé¾î°¡°ÚÁö¸¸, ÀÌ¹ø °úÁ¦¿¡¼­´Â -1·Î ´ëÃ¼ÇÕ´Ï´Ù.
+    ì‹¤ì œ ì‹œìŠ¤í…œì—ì„œëŠ” ìŠ¤íƒì´ ë©”ëª¨ë¦¬ì— ì €ì¥ë˜ì§€ë§Œ, ë³¸ ê³¼ì œì—ì„œëŠ” `int` ë°°ì—´ì„ ì´ìš©í•˜ì—¬ ë©”ëª¨ë¦¬ë¥¼ êµ¬í˜„í•©ë‹ˆë‹¤.
+    ì›ë˜ëŠ” SFPì™€ Return Addressì— ì‹¤ì œ ê°€ìƒ ë©”ëª¨ë¦¬ ì£¼ì†Œê°€ ë“¤ì–´ê°€ê² ì§€ë§Œ, ì´ë²ˆ ê³¼ì œì—ì„œëŠ” -1ë¡œ ëŒ€ì²´í•©ë‹ˆë‹¤.
 
-    int call_stack[]      : ½ÇÁ¦ µ¥ÀÌÅÍ(`int °ª`) ¶Ç´Â `-1` (¸ŞÅ¸µ¥ÀÌÅÍ ±¸ºĞ¿ë)À» ÀúÀåÇÏ´Â int ¹è¿­
-    char stack_info[][]   : call_stack[]°ú °°Àº À§Ä¡(index)¿¡ ´ëÇÑ ¼³¸íÀ» ÀúÀåÇÏ´Â ¹®ÀÚ¿­ ¹è¿­
+    int call_stack[]      : ì‹¤ì œ ë°ì´í„°(`int ê°’`) ë˜ëŠ” `-1` (ë©”íƒ€ë°ì´í„° êµ¬ë¶„ìš©)ì„ ì €ì¥í•˜ëŠ” int ë°°ì—´
+    char stack_info[][]   : call_stack[]ê³¼ ê°™ì€ ìœ„ì¹˜(index)ì— ëŒ€í•œ ì„¤ëª…ì„ ì €ì¥í•˜ëŠ” ë¬¸ìì—´ ë°°ì—´
 
-    ==========================call_stack ÀúÀå ±ÔÄ¢==========================
-    ¸Å°³ º¯¼ö / Áö¿ª º¯¼ö¸¦ pushÇÒ °æ¿ì   : int °ª ±×´ë·Î
-    Saved Frame Pointer ¸¦ pushÇÒ °æ¿ì  : call_stack¿¡¼­ÀÇ index
-    ¹İÈ¯ ÁÖ¼Ò°ªÀ» pushÇÒ °æ¿ì       : -1
+    ==========================call_stack ì €ì¥ ê·œì¹™==========================
+    ë§¤ê°œ ë³€ìˆ˜ / ì§€ì—­ ë³€ìˆ˜ë¥¼ pushí•  ê²½ìš°   : int ê°’ ê·¸ëŒ€ë¡œ
+    Saved Frame Pointer ë¥¼ pushí•  ê²½ìš°  : call_stackì—ì„œì˜ index
+    ë°˜í™˜ ì£¼ì†Œê°’ì„ pushí•  ê²½ìš°       : -1
     =======================================================================
 
 
-    ==========================stack_info ÀúÀå ±ÔÄ¢==========================
-    ¸Å°³ º¯¼ö / Áö¿ª º¯¼ö¸¦ pushÇÒ °æ¿ì        : º¯¼ö¿¡ ´ëÇÑ ¼³¸í
-    Saved Frame Pointer ¸¦ pushÇÒ °æ¿ì  : ¾î¶² ÇÔ¼öÀÇ SFPÀÎÁö
-    ¹İÈ¯ ÁÖ¼Ò°ªÀ» pushÇÒ °æ¿ì                 : "Return Address"
+    ==========================stack_info ì €ì¥ ê·œì¹™==========================
+    ë§¤ê°œ ë³€ìˆ˜ / ì§€ì—­ ë³€ìˆ˜ë¥¼ pushí•  ê²½ìš°        : ë³€ìˆ˜ì— ëŒ€í•œ ì„¤ëª…
+    Saved Frame Pointer ë¥¼ pushí•  ê²½ìš°  : ì–´ë–¤ í•¨ìˆ˜ì˜ SFPì¸ì§€
+    ë°˜í™˜ ì£¼ì†Œê°’ì„ pushí•  ê²½ìš°                 : "Return Address"
     ========================================================================
 */
 #include <stdio.h>
 #include <string.h>
-#define STACK_SIZE 50 // ÃÖ´ë ½ºÅÃ Å©±â
+#define STACK_SIZE 50 // ìµœëŒ€ ìŠ¤íƒ í¬ê¸°
 
-int     call_stack[STACK_SIZE];         // Call StackÀ» ÀúÀåÇÏ´Â ¹è¿­ ex) 2
-char    stack_info[STACK_SIZE][20];     // Call Stack ¿ä¼Ò¿¡ ´ëÇÑ ¼³¸íÀ» ÀúÀåÇÏ´Â ¹è¿­ ex) arg2
+int     call_stack[STACK_SIZE];         // Call Stackì„ ì €ì¥í•˜ëŠ” ë°°ì—´ ex) 2
+char    stack_info[STACK_SIZE][20];     // Call Stack ìš”ì†Œì— ëŒ€í•œ ì„¤ëª…ì„ ì €ì¥í•˜ëŠ” ë°°ì—´ ex) arg2
 
 /*  SP (Stack Pointer), FP (Frame Pointer)
 
-    SP´Â ÇöÀç ½ºÅÃÀÇ ÃÖ»ó´Ü ÀÎµ¦½º¸¦ °¡¸®Åµ´Ï´Ù.
-    ½ºÅÃÀÌ ºñ¾îÀÖÀ» ¶§ SP = -1, ÇÏ³ª°¡ ½×ÀÌ¸é `call_stack[0]` -> SP = 0, `call_stack[1]` -> SP = 1, ...
+    SPëŠ” í˜„ì¬ ìŠ¤íƒì˜ ìµœìƒë‹¨ ì¸ë±ìŠ¤ë¥¼ ê°€ë¦¬í‚µë‹ˆë‹¤.
+    ìŠ¤íƒì´ ë¹„ì–´ìˆì„ ë•Œ SP = -1, í•˜ë‚˜ê°€ ìŒ“ì´ë©´ `call_stack[0]` -> SP = 0, `call_stack[1]` -> SP = 1, ...
 
-    FP´Â ÇöÀç ÇÔ¼öÀÇ ½ºÅÃ ÇÁ·¹ÀÓ Æ÷ÀÎÅÍÀÔ´Ï´Ù.
-    ½ÇÇà ÁßÀÎ ÇÔ¼ö ½ºÅÃ ÇÁ·¹ÀÓÀÇ sfp¸¦ °¡¸®Åµ´Ï´Ù.
+    FPëŠ” í˜„ì¬ í•¨ìˆ˜ì˜ ìŠ¤íƒ í”„ë ˆì„ í¬ì¸í„°ì…ë‹ˆë‹¤.
+    ì‹¤í–‰ ì¤‘ì¸ í•¨ìˆ˜ ìŠ¤íƒ í”„ë ˆì„ì˜ sfpë¥¼ ê°€ë¦¬í‚µë‹ˆë‹¤.
 */
 int SP = -1;
 int FP = -1;
@@ -43,8 +43,8 @@ void func2(int arg1, int arg2);
 void func3(int arg1);
 
 /*
-    ÇöÀç call_stack ÀüÃ¼¸¦ Ãâ·ÂÇÕ´Ï´Ù.
-    ÇØ´ç ÇÔ¼öÀÇ Ãâ·Â °á°úµéÀ» ¹ÙÅÁÀ¸·Î ±¸Çö ¿Ï¼ºµµ¸¦ Æò°¡ÇÒ ¿¹Á¤ÀÔ´Ï´Ù.
+    í˜„ì¬ call_stack ì „ì²´ë¥¼ ì¶œë ¥í•©ë‹ˆë‹¤.
+    í•´ë‹¹ í•¨ìˆ˜ì˜ ì¶œë ¥ ê²°ê³¼ë“¤ì„ ë°”íƒ•ìœ¼ë¡œ êµ¬í˜„ ì™„ì„±ë„ë¥¼ í‰ê°€í•  ì˜ˆì •ì…ë‹ˆë‹¤.
 */
 void print_stack()
 {
@@ -61,7 +61,7 @@ void print_stack()
         if (call_stack[i] != -1)
             printf("%d : %s = %d", i, stack_info[i], call_stack[i]);
         else
-            printf("%d : %s", i, stack_info[i]);    // call_stackÀÌ -1ÀÎ °æ¿ì. Áï, ¸ŞÅ¸µ¥ÀÌÅÍ.
+            printf("%d : %s", i, stack_info[i]);    // call_stackì´ -1ì¸ ê²½ìš°. ì¦‰, ë©”íƒ€ë°ì´í„°.
 
         if (i == SP)
             printf("    <=== [esp]\n");
@@ -74,12 +74,12 @@ void print_stack()
 }
 
 
-//func ³»ºÎ´Â ÀÚÀ¯·Ó°Ô Ãß°¡ÇØµµ ±¦ÂúÀ¸³ª, ¾Æ·¡ÀÇ ±¸Á¶¸¦ ¹Ù²ÙÁö´Â ¸¶¼¼¿ä
+//func ë‚´ë¶€ëŠ” ììœ ë¡­ê²Œ ì¶”ê°€í•´ë„ ê´œì°®ìœ¼ë‚˜, ì•„ë˜ì˜ êµ¬ì¡°ë¥¼ ë°”ê¾¸ì§€ëŠ” ë§ˆì„¸ìš”
 void func1(int arg1, int arg2, int arg3)
 {
     int var_1 = 100;
 
-    // func1ÀÇ ½ºÅÃ ÇÁ·¹ÀÓ Çü¼º (ÇÔ¼ö ÇÁ·Ñ·Î±× + push)
+    // func1ì˜ ìŠ¤íƒ í”„ë ˆì„ í˜•ì„± (í•¨ìˆ˜ í”„ë¡¤ë¡œê·¸ + push)
     SP++;                       // SP = 0
     strcpy_s(stack_info[SP], sizeof(stack_info[SP]), "arg3");
     call_stack[SP] = arg3;
@@ -104,7 +104,7 @@ void func1(int arg1, int arg2, int arg3)
 
     print_stack();
     func2(11, 13);
-    // func2ÀÇ ½ºÅÃ ÇÁ·¹ÀÓ Á¦°Å (ÇÔ¼ö ¿¡ÇÊ·Î±× + pop)
+    // func2ì˜ ìŠ¤íƒ í”„ë ˆì„ ì œê±° (í•¨ìˆ˜ ì—í•„ë¡œê·¸ + pop)
     for (SP; SP > FP; SP--) {
         call_stack[SP] = 0;
     }                           // SP = 9, FP = 9
@@ -117,6 +117,12 @@ void func1(int arg1, int arg2, int arg3)
     call_stack[SP] = 0;
     SP--;                       // SP = 7, FP = 4
 
+    call_stack[SP] = 0;
+    SP--;                       // SP = 6, FP = 4
+
+    call_stack[SP] = 0;
+    SP--;                       // SP = 5, FP = 4
+
     print_stack();
 }
 
@@ -125,7 +131,7 @@ void func2(int arg1, int arg2)
 {
     int var_2 = 200;
 
-    // func2ÀÇ ½ºÅÃ ÇÁ·¹ÀÓ Çü¼º (ÇÔ¼ö ÇÁ·Ñ·Î±× + push)
+    // func2ì˜ ìŠ¤íƒ í”„ë ˆì„ í˜•ì„± (í•¨ìˆ˜ í”„ë¡¤ë¡œê·¸ + push)
     SP++;                       // SP = 6, FP = 4
     strcpy_s(stack_info[SP], sizeof(stack_info[SP]), "arg2");
     call_stack[SP] = arg2;
@@ -147,7 +153,7 @@ void func2(int arg1, int arg2)
 
     print_stack();
     func3(77);
-    // func3ÀÇ ½ºÅÃ ÇÁ·¹ÀÓ Á¦°Å (ÇÔ¼ö ¿¡ÇÊ·Î±× + pop)
+    // func3ì˜ ìŠ¤íƒ í”„ë ˆì„ ì œê±° (í•¨ìˆ˜ ì—í•„ë¡œê·¸ + pop)
     for (SP; SP > FP; SP--) {
         call_stack[SP] = 0;
     }                           // SP = 13, FP = 13
@@ -160,6 +166,9 @@ void func2(int arg1, int arg2)
     call_stack[SP] = 0;
     SP--;                       // SP = 11, FP = 9
 
+    call_stack[SP] = 0;
+    SP--;                       // SP = 10, FP = 9
+
     print_stack();
 }
 
@@ -169,7 +178,7 @@ void func3(int arg1)
     int var_3 = 300;
     int var_4 = 400;
 
-    // func3ÀÇ ½ºÅÃ ÇÁ·¹ÀÓ Çü¼º (ÇÔ¼ö ÇÁ·Ñ·Î±× + push)
+    // func3ì˜ ìŠ¤íƒ í”„ë ˆì„ í˜•ì„± (í•¨ìˆ˜ í”„ë¡¤ë¡œê·¸ + push)
     SP++;                       // SP = 11, FP = 9
     strcpy_s(stack_info[SP], sizeof(stack_info[SP]), "arg1");
     call_stack[SP] = arg1;
@@ -192,11 +201,11 @@ void func3(int arg1)
 }
 
 
-//main ÇÔ¼ö¿¡ °ü·ÃµÈ stack frameÀº ±¸ÇöÇÏÁö ¾Ê¾Æµµ µË´Ï´Ù.
+//main í•¨ìˆ˜ì— ê´€ë ¨ëœ stack frameì€ êµ¬í˜„í•˜ì§€ ì•Šì•„ë„ ë©ë‹ˆë‹¤.
 int main()
 {
     func1(1, 2, 3);
-    // func1ÀÇ ½ºÅÃ ÇÁ·¹ÀÓ Á¦°Å (ÇÔ¼ö ¿¡ÇÊ·Î±× + pop)
+    // func1ì˜ ìŠ¤íƒ í”„ë ˆì„ ì œê±° (í•¨ìˆ˜ ì—í•„ë¡œê·¸ + pop)
     for (SP; SP > FP; SP--) {
         call_stack[SP] = 0;
     }                           // SP = 4, FP = 4
